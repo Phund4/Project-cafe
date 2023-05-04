e750302c69bedc5c718a94add60948f8a9297190
Phunda <phunda73@gmail.com>
2023-05-04 20:52:17 +0300

    .

--- cafe/src/pages/MainPage.jsx
+++ cafe/src/pages/MainPage.jsx
@@ -1,18 +1,20 @@
 import '../sass/mainpage/main.sass'
+import BearMainpage from '../img/Bear-mainpage.svg'
 
 function MainPage() {
     return (
       <>
         <div className='main'>
-            <header className='header-main'>
-
+            <header className='header-mainpage'>
+                <h1 className='header-mainpage__h1'>
+                    Подкрепление
+                </h1>
+                <h1 className='header-mainpage__h1-blur'>
+                    Подкрепление
+                </h1>
+                <img className='header-mainpage__img' src={BearMainpage} alt='Bear'/>
             </header>
-            <h1 className='main__h1'>
-                Подкрепление
-            </h1>
-            <h1 className='main__h1-blur'>
-                Подкрепление
-            </h1>
+            <div className='content-mainpage'></div>
         </div>
       </>
     )
