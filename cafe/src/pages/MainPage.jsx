0301abf9bf2b0043f0c2608114dbed91bbcad6df
Phunda <phunda73@gmail.com>
2023-05-04 19:51:46 +0300

    .

--- cafe/src/pages/MainPage.jsx
+++ cafe/src/pages/MainPage.jsx
@@ -3,7 +3,17 @@ import '../sass/mainpage/main.sass'
 function MainPage() {
     return (
       <>
-        <p>AAAAAAAAAAaa</p>
+        <div className='main'>
+            <header className='header-main'>
+
+            </header>
+            <h1 className='main__h1'>
+                Подкрепление
+            </h1>
+            <h1 className='main__h1-blur'>
+                Подкрепление
+            </h1>
+        </div>
       </>
     )
 }
