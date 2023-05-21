a2f5206b3f4c6721e89edd51b933a700d2061d7e
Andrey Torkhov <torkhovasic@yandex.ru>
2023-05-22 01:21:41 +0300

    15 second branch commit

---
+++ cafe/src/pages/Authorization.jsx
@@ -0,0 +1,49 @@
+import "../sass/authorization/main.sass";
+import LoginBear from "../img/login-bear.svg";
+import Field from "../components/Field/Field";
+import { HashLink as Link } from "react-router-hash-link";
+
+function Authorization() {
+  return (
+    <>
+      <div className="authorization-main">
+        <header className="authorization-header">
+          <img className="authorization-header__img" src={LoginBear} />
+          <h1 className="authorization-header__h1">АВТОРИЗАЦИЯ</h1>
+        </header>
+        <div className="authorization-content">
+          <Field
+            type="text"
+            id="authorization-field-name"
+            text="Ваше имя"
+            placeholder="ИМЯ"
+          />
+          <Field
+            type="tel"
+            id="authorization-field-phone"
+            text="Ваш телефон для связи"
+            placeholder="+7"
+          />
+
+          <Field
+            type="email"
+            id="authorization-field-email"
+            text="Ваша электронная почта"
+          />
+          <Field
+            type="date"
+            id="authorization-field-birthday"
+            text="Дата рождения"
+          />
+        </div>
+        <button className="authorization-button">
+          <Link to="#" className="authorization-button__link">
+            ВОЙТИ
+          </Link>
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Authorization;
--- cafe/src/pages/Login.jsx
+++ cafe/src/pages/Login.jsx
@@ -1,29 +1,34 @@
-import '../sass/login/main.sass'
-import LoginBear from '../img/login-bear.svg'
-import Field from '../components/Field/Field'
+import "../sass/login/main.sass";
+import LoginBear from "../img/login-bear.svg";
+import Field from "../components/Field/Field";
 import { HashLink as Link } from "react-router-hash-link";
 function Login() {
-    return (
+  return (
     <>
-        <div className="login-main">
-            <header className="login-header">
-                <img className='login-header__img' src={LoginBear}/>
-                <h1 className='login-header__h1'>ВХОД</h1>
-            </header>
-            <div className='login-content'>
-                <Field id='login-field-email' text='Почта'/>
-                <Field id='login-field-phone' text='Ваш телефон для связи'/>
-                <p className='login-content__registration'>Регистрация</p>
-            </div>
-            <button className='login-button'>
-                <Link to='/login/error' className="login-button__link">
-                    ВОЙТИ
-                </Link>
-            </button>
-
+      <div className="login-main">
+        <header className="login-header">
+          <img className="login-header__img" src={LoginBear} />
+          <h1 className="login-header__h1">ВХОД</h1>
+        </header>
+        <div className="login-content">
+          <Field type="email" id="login-field-email" text="Почта" />
+          <Field
+            type="tel"
+            id="login-field-phone"
+            text="Ваш телефон для связи"
+          />
+          <Link to="/authorization/" className="login-content__link">
+            <p className="login-content__registration">Регистрация</p>
+          </Link>
         </div>
+        <button className="login-button">
+          <Link to="/login/error" className="login-button__link">
+            ВОЙТИ
+          </Link>
+        </button>
+      </div>
     </>
-    );
+  );
 }
 
 export default Login;
